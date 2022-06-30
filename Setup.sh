f0e11d10b51a12e9815efa1bc6e044768a3a12dc
Tsanfer <a1124851454@gmail.com>
2022-06-29 23:18:48 -0700

    测试小皮面板安装

--- Setup.sh
+++ Setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # 文件位置：$HOME/Setup.sh
 
-github_repo="github.com"
+github_repo="github.daocloudcom"
 github_download="github.com"
 github_raw="raw.githubusercontent.com"
 
@@ -9,7 +9,7 @@ function github_proxy_set() {
   while true; do
     read -r -p "是否启用 Github 国内加速? [Y/n] " input
     case $input in
-    [yY][eE][sS] | [yY])
+    [yY])
       # git config --global url."https://hub.fastgit.xyz/".insteadOf https://github.com/
       github_repo="mirror.ghproxy.com/github.com"
       github_download="mirror.ghproxy.com/github.com"
@@ -23,7 +23,7 @@ function github_proxy_set() {
       break
       ;;
 
-    [nN][oO] | [nN])
+    [nN])
       # git config --global --remove-section url."https://hub.fastgit.xyz/"
       github_repo="github.com"
       github_download="github.com"
@@ -51,7 +51,6 @@ function app_install() {
       git clone https://${github_repo}/dylanaraps/neofetch &&
         make -C ~/neofetch install
     fi
-
   neofetch
   read -r -p "按回车键继续"
 }
@@ -61,8 +60,8 @@ function term_config() {
   echo "---------- 配置终端 ----------"
   echo
   chsh -s $(which zsh) &&
-    RUNZSH=no sh -c "$(curl -fsSL https://${github_raw}/ohmyzsh/ohmyzsh/master/tools/install.sh)" &&
-    git clone https://${github_repo}/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions &&
+    RUNZSH=no sh -c "$(curl -fsSL https://${github_raw}/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  git clone https://${github_repo}/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions &&
     git clone https://${github_repo}/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting &&
     sed -i 's/^plugins=(/plugins=(\nzsh-autosuggestions\nzsh-syntax-highlighting\n/g' ~/.zshrc &&
     sudo wget https://${github_download}/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh &&
@@ -72,31 +71,47 @@ function term_config() {
     unzip ~/.poshthemes/themes.zip -d ~/.poshthemes &&
     chmod u+rw ~/.poshthemes/*.omp.* &&
     rm ~/.poshthemes/themes.zip &&
-    sed -i '$a\eval "$(oh-my-posh --init --shell zsh --config ~/.poshthemes/craver.omp.json)"' ~/.zshrc &&
-    wget https://${github_raw}/Tsanfer/Setup_server/main/.vimrc -P ~
+    sed -i '$a\eval "$(oh-my-posh --init --shell zsh --config ~/.poshthemes/craver.omp.json)"' ~/.zshrc
+  wget https://${github_raw}/Tsanfer/Setup_server/main/.vimrc -P ~
 }
 
 function docker_install() {
   echo
   echo "---------- 安装 Docker ----------"
   echo
-  # sudo apt-get remove docker docker-engine docker.io containerd runc && \
-  sudo apt-get install \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release -y &&
-    sudo mkdir -p /etc/apt/keyrings &&
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg &&
-    echo \
-      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  echo "检查 Docker 状态..."
+  docker -v
+  if (($? != 0)); then
+    release_ver=$(awk '/Ubuntu/ {print $2}' /etc/issue | awk -F. '{printf "%s.%s\n",$1,$2}')
+    echo "$(echo $release_ver | bc) >= 18.04" | bc
+    if (($? != 0)); then
+      echo "安装 Docker 环境..."
+      # sudo apt-get remove docker docker-engine docker.io containerd runc && \
+      sudo apt-get install \
+        ca-certificates \
+        curl \
+        gnupg \
+        lsb-release -y &&
+        sudo mkdir -p /etc/apt/keyrings &&
+        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg &&
+        echo \
+          "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null &&
-    sudo apt-get update -y &&
-    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+        sudo apt-get update -y &&
+        sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+      echo "安装docker环境...安装完成!"
+      docker_install && docker_deploy
+    else
+      echo "The Ubuntu version is lower than 18.04, can't install docker."
+    fi
+  else
+    echo "检查到 Docker 已安装!"
+  fi
+
 }
 
 function swap_set() {
-  free -m
+  free -h
 
   while true; do
     read -r -p "关闭还是启用 swap 功能? (Y:启用/n:关闭/q:退出): " input
@@ -135,46 +150,67 @@ function docker_deploy() {
   echo
   echo "---------- 运行 Docker-compose ----------"
   echo
-  wget https://${github_raw}/Tsanfer/Setup_server/main/docker-compose.yml -P ~ &&
-    # docker compose up -d &&
-    PS3="选择需要安装的 Docker 容器: "
-  docker_list=("code-server" "halo-blog" "Quit")
-  select compose in "${docker_list[@]}"; do
-    case $compose in
-    "code-server")
-      read -s -p "设置密码: " password
-      read -s -p "设置 sudo 密码: " sudo_password
-      echo "PASSWORD=$password" >>~/$compose.env
-      echo "SUDO_PASSWORD=$sudo_password" >>~/$compose.env
-      wget https://${github_raw}/Tsanfer/Setup_server/main/$compose.yml -P ~ &&
-        docker compose -f ~/$compose.yml --env-file ~/$compose.env up -d &&
+  echo "检查 Docker 状态..."
+  docker -v
+  if (($? != 0)); then
+    wget https://${github_raw}/Tsanfer/Setup_server/main/docker-compose.yml -P ~ &&
+      # docker compose up -d &&
+      PS3="选择需要安装的 Docker 容器: "
+    docker_list=("code-server" "Quit")
+    select compose in "${docker_list[@]}"; do
+      case $compose in
+      "code-server")
+        read -s -p "设置密码: " password
+        read -s -p "设置 sudo 密码: " sudo_password
+        echo "PASSWORD=$password" >>~/$compose.env
+        echo "SUDO_PASSWORD=$sudo_password" >>~/$compose.env
+        wget https://${github_raw}/Tsanfer/Setup_server/main/$compose.yml -P ~ &&
+          docker compose -f ~/$compose.yml --env-file ~/$compose.env up -d &&
+          break
+        ;;
+      "Quit")
+        echo "退出"
         break
-      ;;
-    "halo-blog")
-      wget https://${github_raw}/Tsanfer/Setup_server/main/$compose.yml -P ~ &&
-        docker compose -f ~/$compose.yml up -d &&
-        break
-      ;;
-    "Quit")
-      echo "退出"
+        ;;
+      *) echo "错误选项：$REPLY" ;;
+      esac
+    done
+    docker ps
+  else
+    echo "Docker 未安装!"
+  fi
+}
+
+function phpstudy_install() {
+  while true; do
+    read -r -p "是否安装小皮面板？[Y/n]: " input
+    case $input in
+    [yY])
+      wget -O install.sh https://download.xp.cn/install.sh && sudo bash install.sh
       break
       ;;
-    *) echo "错误选项：$REPLY" ;;
+
+    [nN])
+      break
+      ;;
+
+    *)
+      echo "Invalid input..."
+      ;;
     esac
   done
-  docker ps
 }
 
 github_proxy_set
 
-if grep "Ubuntu" /etc/issue; then
-  app_install && term_config
-  release_ver=$(awk '/Ubuntu/ {print $2}' /etc/issue | awk -F. '{printf "%s.%s\n",$1,$2}')
-  if (($(echo "$(echo $release_ver | bc) >= 18.04" | bc))); then
-    swap_set && docker_install && docker_deploy
-  else
-    echo "The Ubuntu version is lower than 18.04, can't install docker."
-  fi
+grep "Ubuntu" /etc/issue
+if [ $? -eq 0 ]; then
+  app_install &&
+    term_config &&
+    phpstudy_install &&
+    swap_set
+  phpstudy_install || docker_install
+  docker_deploy
   echo "Done!!!"
   zsh
 else
